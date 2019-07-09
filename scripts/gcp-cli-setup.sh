06eefd22c351239d6c1750c88e9faf99273160c2
henrychia12 <henry_chia21@hotmail.co.uk>
2019-07-09 22:08:09 +0000

    default region and zone set in gcp cli setup

--- scripts/gcp-cli-setup.sh
+++ scripts/gcp-cli-setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # set up google cli
-#curl https://sdk.cloud.google.com | bash
+curl https://sdk.cloud.google.com | bash
 
 # installing jq
-#sudo apt-get install jq
+sudo apt-get install jq
+
+# setting default region - europe west
+gcloud compute project-info add-metadata --metadata google-compute-default-region=europe-west1,google-compute-default-zone=europe-west1-b
 
 # inserting environment variables into .bashrc
 cp ../jq-variables ~/.gcloud_vars
